542b27173629be9eb3bd2a21bb107af2b5c6f803
Deyse Carneiro <44411176+deecarneiro@users.noreply.github.com>
2022-04-24 20:21:57 -0300

    Mobile slide indicator for card of services

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,14 +3,19 @@ import './Card.css';
 
 function Card(props) {
 
-    const {title, imgSrc, description, buttonValue, onclick} = props;
+    const { title, imgSrc, description, buttonValue, onclick, visibility } = props;
     return (
-        <span className="card">
-            <h1>{title}</h1>
-            {props.children}
-            <p>{description}</p>
-            <button onClick={onclick}>{buttonValue}</button>
-        </span>
+        <>
+            {
+                visibility &&
+                    <span className="card">
+                        < h1 > {title}</h1 >
+                        {props.children}
+                        < p > {description}</p >
+                        <button onClick={onclick}>{buttonValue}</button>
+                    </span >
+            }
+        </>
     );
 }
 
--- src/components/Section/Sections/SectionServices/index.js
+++ src/components/Section/Sections/SectionServices/index.js
@@ -8,74 +8,93 @@ import REST from '../../../../assets/images/REST.jsx';
 import ReactCardCarousel from "react-card-carousel";
 import VisibilitySensor from 'react-visibility-sensor';
 import { useState } from 'react';
-import { BrowserView, MobileView } from 'react-device-detect';
+import { isMobile } from 'react-device-detect';
 import Games from '../../../../assets/images/Games';
 
 function SectionServices() {
 
   const [visible, setVisible] = useState(false);
   const [isTyping, setIsTyping] = useState(true);
+  const [currentCard, setCurrentCard] = useState(0);
 
   const goTo = () => {
     return document.location = '#contact'
   }
 
-  return (
-    <Section>
-      <div id="services">
-        {isTyping ?
-        <Typewriter
-          onInit={(typewriter) => {
-            typewriter.typeString('cd /Work/Services')
-              .callFunction(() => {
-                console.log('cd /Work/Services');
-              })
-              .pauseFor(1000)
-              .deleteAll()
-              .start()
-            typewriter.typeString('ls')
-              .callFunction(() => {
-                setVisible(true)
-                setIsTyping(false)
-              })
-              .pauseFor(2500)
-              .deleteAll()
-              .callFunction(() => {
-                console.log('All strings were deleted');
-              })
-              .start()
-          }}
-        /> : ''}
-        {visible &&
-          <div className="section-services">
-            <Card
-              title="Mobile"
-              description="Frontend mobile with React Native, Flutter, Ionic Framework..."
-              buttonValue="See more"
-              onclick={() => goTo()}
-            >
-              <Mobile />
-            </Card>
-            <Card
-              title="Web"
-              description="Frontend and Backend development"
-              buttonValue="See more"
-              onclick={() => goTo()}
-            >
-              <Web />
-            </Card>
-            <Card
-              title="Games"
-              description="REST API development with Spring Boot, Node.js and JPA"
-              buttonValue="See more"
-              onclick={() => goTo()}
-            >
-              <Games />
-            </Card>
-          </div>}
-      </div>
 
-    </Section>
+
+  const cards = [
+    {
+      title: "Mobile",
+      description: "Frontend mobile with React Native, Flutter, Ionic Framework...",
+      icon: <Mobile />
+
+    },
+    {
+      title: "Web",
+      description: "Frontend and Backend development",
+      icon: <Web />
+    },
+    {
+      title: "Games",
+      description: "Frontend and Backend development",
+      icon: <Games />
+    }
+  ]
+
+  return (
+    <VisibilitySensor onChange={(isVisible) => { setVisible(isVisible) }}>
+      <Section>
+        {visible ?
+
+          <div id="services">
+            {isTyping ?
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter.typeString('cd /Work/Services')
+                    .callFunction(() => {
+                      console.log('cd /Work/Services');
+                    })
+                    .pauseFor(1000)
+                    .deleteAll()
+                    .start()
+                  typewriter.typeString('ls')
+                    .callFunction(() => {
+                      setVisible(true)
+                      setIsTyping(false)
+                    })
+                    .pauseFor(2500)
+                    .deleteAll()
+                    .callFunction(() => {
+                      console.log('All strings were deleted');
+                    })
+                    .start()
+                }}
+              /> : ''}
+            {isTyping == false &&
+              <div className="section-services">
+                {cards.map((card, key) => {
+                  return <Card
+                    title={card.title}
+                    description={card.description}
+                    buttonValue="See more"
+                    onclick={() => goTo()}
+                    key={key}
+                    visibility={key === currentCard}
+                  >
+                    {card.icon}
+                  </Card>
+                })}
+                {isMobile && 
+                 cards.map((card, key) => {
+                  return <a onClick={() => {setCurrentCard(key)}}></a>
+                })}
+              </div>
+            }
+          </div> : ''}
+      </Section>
+    </VisibilitySensor >
+
   );
 }
 
