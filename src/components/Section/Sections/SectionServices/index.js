cba36999780a1ccbbb513a6cb8fa84011a732984
Deyse Carneiro <44411176+deecarneiro@users.noreply.github.com>
2022-04-26 17:00:11 -0300

    Adding hotfix for mobile version broken typing

--- src/components/Section/Sections/SectionServices/index.js
+++ src/components/Section/Sections/SectionServices/index.js
@@ -122,7 +122,24 @@ function SectionServices() {
               <div className="section-services" >
                {browser}
               </div>
-              : ''
+              : isMobile &&
+              <div className="section-services"   {...handlers}>
+                {cards.map((card, key) => {
+                  return <MobileCard
+                    title={card.title}
+                    description={card.description}
+                    buttonValue="See more"
+                    onclick={() => goTo()}
+                    key={key}
+                    visibility={key === currentCard}
+                  >
+                    {card.icon}
+                  </MobileCard>
+                })}
+                {cards.map((card, key) => {
+                  return <a onClick={() => { setCurrentCard(key) }}></a>
+                })}
+              </div>
              }
           </div> : ''}
       </Section>
