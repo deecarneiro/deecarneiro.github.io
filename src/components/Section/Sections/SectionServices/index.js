93c1489afc545dd84129363692f4736a3aa667c3
Deyse Carneiro <44411176+deecarneiro@users.noreply.github.com>
2022-05-09 14:27:20 -0300

    Hotfix- Removing typewriting

--- src/components/Section/Sections/SectionServices/index.js
+++ src/components/Section/Sections/SectionServices/index.js
@@ -6,7 +6,7 @@ import MobileCard from '../../../MobileCard';
 import Mobile from '../../../../assets/images/Mobile.jsx';
 import Web from '../../../../assets/images/Web.jsx';
 import VisibilitySensor from 'react-visibility-sensor';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { isMobile } from 'react-device-detect';
 import Games from '../../../../assets/images/Games';
 import { useSwipeable } from 'react-swipeable';
@@ -16,6 +16,7 @@ function SectionServices() {
   const [visible, setVisible] = useState(false);
   const [isTyping, setIsTyping] = useState(true);
   const [currentCard, setCurrentCard] = useState(0);
+  const [isMobileDetected, setIsMobileDetected] = useState(false)
 
   const goTo = () => {
     return document.location = '#contact'
@@ -70,61 +71,42 @@ function SectionServices() {
     </Card>
   })
 
+  useEffect(() => {
+    setIsMobileDetected(isMobile)
+  }, []);
+
   return (
     <VisibilitySensor onChange={(isVisible) => { setVisible(isVisible) }}>
       <Section>
         {visible ?
-
           <div id="services">
-            {isTyping ?
-              <Typewriter
-                onInit={(typewriter) => {
-                  typewriter.typeString('cd /Work/Services')
-                    .callFunction(() => {
-                      console.log('cd /Work/Services');
-                    })
-                    .pauseFor(1000)
-                    .deleteAll()
-                    .start()
-                  typewriter.typeString('ls')
-                    .callFunction(() => {
-                      setVisible(true)
-                      setIsTyping(false)
-                    })
-                    .pauseFor(2500)
-                    .deleteAll()
-                    .callFunction(() => {
-                      console.log('All strings were deleted');
-                    })
-                    .start()
-                }}
-              /> : ''}
-            {isTyping === false ?
-              isMobile ?
-              <div className="section-services"   {...handlers}>
-                {cards.map((card, key) => {
-                  return <MobileCard
-                    title={card.title}
-                    description={card.description}
-                    buttonValue="See more"
-                    onclick={() => goTo()}
-                    key={key}
-                    visibility={key === currentCard}
-                  >
-                    {card.icon}
-                  </MobileCard>
-                })}
-                {cards.map((card, key) => {
-                  return <a onClick={() => { setCurrentCard(key) }}></a>
-                })}
-              </div>
-              :
-              <div className="section-services" >
-               {browser}
-              </div>
-              : ""
-             }
-          </div> : ''}
+            {
+              isMobileDetected ?
+                <div className="section-services"   {...handlers}>
+                  {cards.map((card, key) => {
+                    return <MobileCard
+                      title={card.title}
+                      description={card.description}
+                      buttonValue="See more"
+                      onclick={() => goTo()}
+                      key={key}
+                      visibility={key === currentCard}
+                    >
+                      {card.icon}
+                    </MobileCard>
+                  })}
+                  {cards.map((card, key) => {
+                    return <a onClick={() => { setCurrentCard(key) }}></a>
+                  })}
+                </div>
+                :
+                <div className="section-services" >
+                  {browser}
+                </div>
+            }
+          </div>
+          : ""
+        }
       </Section>
     </VisibilitySensor >
 
